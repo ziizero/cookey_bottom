6bd5f5ee86186d6011cb9c870a8748c02c4f1415
jiyeong <jyfullsun42@gmail.com>
2023-11-23 16:17:01 +0900

    수정 1차

--- app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
+++ app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
@@ -1,29 +1,32 @@
 package kr.ac.duksung.cookey_bottom;
 
-
-
-
 import android.os.Bundle;
-
+import android.widget.CalendarView;
+import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomappbar.BottomAppBar;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomAppBar bottomAppBar;
-    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CalendarView calendarView = findViewById(R.id.calendarView);
 
+        // Set the header text color programmatically
+        setCalendarHeaderTextColor(calendarView, android.R.color.primary_text_light);
+    }
+
+    private void setCalendarHeaderTextColor(CalendarView calendarView, @ColorRes int colorRes) {
+        int color = ContextCompat.getColor(this, colorRes);
+        try {
+            java.lang.reflect.Field field = calendarView.getClass().getDeclaredField("mHeaderTextColor");
+            field.setAccessible(true);
+            field.set(calendarView, color);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
