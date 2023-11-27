2631dd20eb2a4ef8b9f45b6ec0e36e832dcc45ea
jiyeong <jyfullsun42@gmail.com>
2023-11-27 23:59:01 +0900

    시간 포화도

--- app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
+++ app/src/main/java/kr/ac/duksung/cookey_bottom/MainActivity.java
@@ -5,8 +5,15 @@ import android.widget.CalendarView;
 import androidx.annotation.ColorRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView timeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         CalendarView calendarView = findViewById(R.id.calendarView);
+        timeTextView = findViewById(R.id.timeTextView);
 
         // Set the header text color programmatically
         setCalendarHeaderTextColor(calendarView, android.R.color.primary_text_light);
+
+        // Display the current time
+        displayCurrentTime();
     }
 
     private void setCalendarHeaderTextColor(CalendarView calendarView, @ColorRes int colorRes) {
@@ -28,5 +39,15 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void displayCurrentTime() {
+        // 현재 시간을 받아옴
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm", Locale.getDefault()); // 24시간 형식
+        String currentTime = sdf.format(calendar.getTime());
+
+        // 받아온 현재 시간을 TextView에 표시
+        timeTextView.setText(currentTime);
     }
 }
